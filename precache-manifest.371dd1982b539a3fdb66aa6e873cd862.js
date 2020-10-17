af00de2e00d8bf62c74b3ea746b4d0ddace3981b
Nikki <npeel3497@gmail.com>
2020-10-17 11:27:39 -0400

    Updates

---
+++ precache-manifest.371dd1982b539a3fdb66aa6e873cd862.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a59d037106dae50e678fde892676b443",
+    "url": "/tailwind-gallery/index.html"
+  },
+  {
+    "revision": "b10c611262476b0483fe",
+    "url": "/tailwind-gallery/static/css/main.132e8dd7.chunk.css"
+  },
+  {
+    "revision": "5c45fef4dbb3cf765ad3",
+    "url": "/tailwind-gallery/static/js/2.e7ed26ff.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tailwind-gallery/static/js/2.e7ed26ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10c611262476b0483fe",
+    "url": "/tailwind-gallery/static/js/main.769afb2d.chunk.js"
+  },
+  {
+    "revision": "1bc71378144d6744c618",
+    "url": "/tailwind-gallery/static/js/runtime-main.569aac49.js"
+  }
+]);
